bf226be6e94b9c33838dda8cd9cbcc5d30052277
4r5yn <110546324+4r5yn@users.noreply.github.com>
2023-10-17 00:18:00 +0530

    Update variables.tf

--- create_instance/variables.tf
+++ create_instance/variables.tf
@@ -5,10 +5,9 @@ variable "AWS_SECRET_KEY" {
   default = "your_value"
 }
 variable "AWS_REGION" {
-   default = "us_east_1"
+   default = "us-east-1"
 }
 variable "AMIS" {
-    type = "map"
     default = {
         us-east-1 = "ami-13be557e"
     }
